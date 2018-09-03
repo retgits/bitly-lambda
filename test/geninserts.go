593710022ddf2ecad7c7cd5ab6eced61a37ef84a
retgits <lstigter@gmail.com>
2018-09-02 18:01:45 -0700

    Added utility to create insert statements

--- test/geninserts.go
+++ test/geninserts.go
@@ -1,24 +1,16 @@
-// Package main is the main implementation of the Bitly serverless app.
-//
-// The app retrieves statistics on the various bitlinks associated with the account of the
-// authenticated user
+// Package main is helper...
 package main
 
-// The imports
 import (
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
-	"path/filepath"
 	"strings"
 	"time"
 
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/retgits/bitly-lambda/database"
 	"github.com/retgits/bitly-lambda/util"
 )
 
@@ -26,27 +18,15 @@ import (
 const (
 	// The name of the Bitly Generic Access Token parameter in Amazon SSM
 	tokenName = "/bitly/apptoken"
-	// The name of the database file
-	databaseName = "bitly-stats.db"
 )
 
 // Variables
 var (
 	// The region in which the Lambda function is deployed
 	awsRegion = util.GetEnvKey("region", "us-west-2")
-	// The name of the bucket that has the csv file
-	s3Bucket = util.GetEnvKey("s3Bucket", "retgits-bitly")
-	// The temp folder to store the database file while working
-	tempFolder = util.GetEnvKey("tempFolder", ".")
 )
 
-// The handler function is executed every time that a new Lambda event is received.
-// It takes a JSON payload (you can see an example in the event.json file) and only
-// returns an error if the something went wrong. The event comes fom CloudWatch and
-// is scheduled every interval (where the interval is defined as variable)
-func handler(request events.CloudWatchEvent) error {
-	log.Printf("Processing Lambda request [%s]", request.ID)
-
+func main() {
 	// Prepare a channel for done
 	done := make(chan struct{})
 	linksChan := make(chan map[string]interface{})
@@ -60,28 +40,7 @@ func handler(request events.CloudWatchEvent) error {
 	// Get the Bitly Generic Access Token
 	bitlyToken, err := util.GetSSMParameter(awsSession, tokenName, true)
 	if err != nil {
-		return err
-	}
-
-	// Make a backup of the latest version of the bitly stats
-	err = util.CopyFile(awsSession, databaseName, s3Bucket)
-	if err != nil {
-		log.Println(err.Error())
-		return err
-	}
-
-	// Download the latest version of the bitly stats
-	err = util.DownloadFile(awsSession, tempFolder, databaseName, s3Bucket)
-	if err != nil {
-		log.Println(err.Error())
-		return err
-	}
-
-	// Create a connection to the SQLite database
-	dbase, err := database.New(filepath.Join(tempFolder, databaseName))
-	if err != nil {
-		log.Printf("Error while connecting to the database: %s\n", err.Error())
-		return err
+		panic(err)
 	}
 
 	// Get the groups associated with the current account. There should be only one group for a free account
@@ -89,7 +48,7 @@ func handler(request events.CloudWatchEvent) error {
 	response, err := util.HTTPRequest("https://api-ssl.bitly.com/v4/groups", httpHeader)
 	if err != nil {
 		log.Printf("Error while connecting to Bitly: %s\n", err.Error())
-		return err
+		panic(err)
 	}
 
 	bitlyGroup := response.Body["groups"].([]interface{})[0].(map[string]interface{})["guid"].(string)
@@ -99,7 +58,7 @@ func handler(request events.CloudWatchEvent) error {
 	response, err = util.HTTPRequest(fmt.Sprintf("https://api-ssl.bitly.com/v4/groups/%s/bitlinks", bitlyGroup), httpHeader)
 	if err != nil {
 		log.Printf("Error while retrieving bitlinks: %s\n", err.Error())
-		return err
+		panic(err)
 	}
 	links := response.Body["links"].([]interface{})
 
@@ -137,11 +96,7 @@ func handler(request events.CloudWatchEvent) error {
 						return err
 					}
 					queryParams, _ := url.ParseQuery(u.RawQuery)
-					err = dbase.Exec(fmt.Sprintf("insert into links (host, path, date, link, url, clicks, utm_source, utm_medium, utm_campaign, utm_term, utm_content) values (\"%s\",\"%s\",\"%v\",\"%s\",\"%s\",\"%d\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\");", u.Host, u.Path, clicks["date"], link["link"], link["long_url"], clicks["clicks"], getValue(queryParams["utm_source"], ""), getValue(queryParams["utm_medium"], ""), getValue(queryParams["utm_campaign"], ""), getValue(queryParams["utm_term"], ""), getValue(queryParams["utm_content"], "")))
-					if err != nil {
-						log.Printf("Error while inserting data into the database: %s\n", err.Error())
-						return err
-					}
+					fmt.Printf("insert into links (host, path, date, link, url, clicks, utm_source, utm_medium, utm_campaign, utm_term, utm_content) values (\"%s\",\"%s\",\"%v\",\"%s\",\"%s\",\"%d\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\");\n", u.Host, u.Path, clicks["date"], link["link"], link["long_url"], clicks["clicks"], getValue(queryParams["utm_source"], ""), getValue(queryParams["utm_medium"], ""), getValue(queryParams["utm_campaign"], ""), getValue(queryParams["utm_term"], ""), getValue(queryParams["utm_content"], ""))
 				}
 			}
 
@@ -154,12 +109,6 @@ func handler(request events.CloudWatchEvent) error {
 
 	// Wait for all links to be processed
 	<-done
-
-	// Upload the latest version of the SQLite database to S3
-	util.UploadFile(awsSession, tempFolder, databaseName, s3Bucket)
-	dbase.Close()
-
-	return nil
 }
 
 func getValue(value []string, fallback string) string {
@@ -168,9 +117,4 @@ func getValue(value []string, fallback string) string {
 	}
 
 	return value[0]
-}
-
-// The main method is executed by AWS Lambda and points to the handler
-func main() {
-	lambda.Start(handler)
 }
